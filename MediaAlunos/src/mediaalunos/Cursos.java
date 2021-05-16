559d6c63419daa506a333959315e7f98eb9ff59b
Rafabreik <72153161+Rafabreik@users.noreply.github.com>
2021-05-15 21:19:48 -0300

    updating vectors

--- MediaAlunos/src/mediaalunos/Cursos.java
+++ MediaAlunos/src/mediaalunos/Cursos.java
@@ -7,45 +7,104 @@ package mediaalunos;
 
 import java.util.Scanner;
 
-
 /**
  *
  * @author RAFA-DESK
  */
 public class Cursos {
-    public int Total_alunos;
-    public int Bimestre;
-    final double Media = 7;
-    private String Nome_curso = " ";
-    
-    Scanner x = new Scanner (System.in);
-    
-  
-    
-    public int total_Bimestre(){
-        
-        System.out.println("Digite a Quantidade de Bimestres ?");
-        this.Bimestre = x.nextInt();
-        return Bimestre;
+
+    Scanner input = new Scanner(System.in);
+
+    private int Total_alunos;
+    private int Bimestre;
+    final double Nota_minima = 7;
+    public String Nome_curso;
+
+    public Cursos(String Nome_curs, int Tot_alunos, int bim) {//Este é o metodo construtor
+        this.setBimestre(bim);
+        this.setTotal_alunos(Tot_alunos);
+        this.setNome_curso(Nome_curs);
+        double not_minima = Nota_minima;
+
+    }
+
+    //Parte de Vetores pra armanamento de Nomes de alunos e notas e media dos mesmos.
+    /*String N_alunos[] = new String[this.Total_alunos];
+    double notas_alunos[][] = new double[this.Total_alunos][this.Bimestre];
+    double media_alunos[] = new double[this.Total_alunos];*/
+
+    public int getTotal_alunos() {
+        return this.Total_alunos;
+
+    }
+
+    public void setTotal_alunos(int Tot_alunos) {
+        this.Total_alunos = Tot_alunos;
+
+    }
+
+    public int getBimestre() {
+        return this.Bimestre;
+
+    }
+
+    public void setBimestre(int Bim) {
+        this.Bimestre = Bim;
+
+    }
+
+    public String getNome_curso() {
+        return this.Nome_curso;
+
+    }
+
+    public void setNome_curso(String Nome_curs) {
+        this.Nome_curso = Nome_curs;
+
+    }
+
+    public void status() {
+        System.out.println("Sobre este curso");
+        System.out.println("Nome do curso " + this.getNome_curso());
+        System.out.println("Numeros de Bimestres " + this.getBimestre());
+        System.out.println("Numero de Alunos do Curso " + this.getTotal_alunos());
+    }
+
+    void Nomes_alunos(String N_alunos[]) {
+        for (int i = 0; i < getTotal_alunos(); i++) {
+            System.out.println("Digite o nome do aluno");
+            N_alunos[i] = input.next();
+
+        }
+    }
+    void Notas_alunos(double[][]notas_alunos,String[]N_alunos) {
+        for (int i = 0; i < getTotal_alunos(); i++) {
+            for (int j = 0; j < getBimestre(); j++) {
+                System.out.println("Digite a Nota do Aluno " + N_alunos[i] + " do " + (j + 1) + " Bimestre");
+                notas_alunos[i][j] = input.nextDouble();
+            }
+        }
+    }
+
+    void Media_alunos(double[][]notas_alunos,String[]N_alunos,double[]media_alunos) {
+        for(int i = 0; i < getTotal_alunos();i++){
+            for(int j = 0; j < getBimestre();j++){
+                media_alunos[i] += notas_alunos[i][j];
+            }
+            media_alunos[i]/= getBimestre();
+        }
+     
+    }
+
+    void resultado(double[]media_alunos,String[]N_alunos) {
+        for(int i = 0; i < getTotal_alunos();i++){
+            if (media_alunos[i] >= Nota_minima){
+                System.out.println("Aluno " + N_alunos[i] + " Media " + media_alunos[i] + " Situação Aprovado!!!");
+            }else{
+                 System.out.println("Aluno " + N_alunos[i] + " Media " + media_alunos[i] + " Situação Reprovado!!!");
+            }
+            
+        }
+    }
 
 }
-    public int alunos(){
-        
-        System.out.println("Digite a Quantidade de Alunos ?");
-        this.Total_alunos = x.nextInt();
-        return Total_alunos ;
-        
-    }
-    public String N_curso(){
-        System.out.println("Digite o Nome do Curso ? ");
-        this.Nome_curso = x.next();
-        
-        return Nome_curso;
-        
-    }
-    
-
-    
-    
-}
-
--- MediaAlunos/src/mediaalunos/MediaAlunos.java
+++ MediaAlunos/src/mediaalunos/MediaAlunos.java
@@ -5,8 +5,6 @@
  */
 package mediaalunos;
 
-import java.util.Scanner;
-
 /**
  *
  * @author RAFA-DESK
@@ -18,68 +16,27 @@ public class MediaAlunos {
      */
     public static void main(String[] args) {
 
-        Cursos c1 = new Cursos();
-
-        c1.N_curso();
+        Cursos c1 = new Cursos("Cia",2,2);
         
-        c1.total_Bimestre();
-
-        c1.alunos();
-
-        String N_alunos[] = new String[c1.Total_alunos];
-        double notas_alunos[][] = new double[c1.Total_alunos][c1.Bimestre];
-        double media_alunos[] = new double[c1.Total_alunos];
-
-        vetor1(N_alunos);
-
-        vetor2(N_alunos, notas_alunos, c1.Total_alunos, c1.Bimestre);
-
-        vetor3(notas_alunos, c1.Total_alunos, c1.Bimestre, media_alunos);
-
-        mostrar_resultado(c1.Total_alunos, media_alunos, c1.Media, N_alunos);
-
         
-    }
-
-    public static void vetor1(String N_alunos[]) {
-        Scanner x = new Scanner(System.in);
-        for (int i = 0; i < N_alunos.length; i++) {
-            System.out.println("Digite o Nome do " + (i + 1) + " Aluno");
-            N_alunos[i] = x.next();
-        }
-
-    }
-
-    private static void vetor2(String[] N_alunos, double[][] notas_alunos, int Total_alunos, int Bimestre) {
-        Scanner x = new Scanner(System.in);
-
-        for (int i = 0; i < Total_alunos; i++) {
-            for (int j = 0; j < Bimestre; j++) {
-                System.out.println("Digite as Notas do " + N_alunos[i] + " do " + (j + 1) + "°Bimestre");
-                notas_alunos[i][j] = x.nextDouble();
-
-            }
-        }
-    }
-
-    private static void vetor3(double[][] notas_alunos, int Total_alunos, int Bimestre, double[] media_alunos) {
-        for (int i = 0; i < Total_alunos; i++) {
-            for (int j = 0; j < Bimestre; j++) {
-                media_alunos[i] += notas_alunos[i][j];
-            }
-            media_alunos[i] /= Bimestre;
-        }
-    }
-
-    private static void mostrar_resultado(int Total_alunos, double[] media_alunos, double Media, String N_alunos[]) {
-        for (int i = 0; i < Total_alunos; i++) {
-
-            if (media_alunos[i] >= Media) {
-                System.out.println("Aluno " + N_alunos[i] + " Media " + media_alunos[i] + " Situação Aprovado!!!");
-            } else {
-               System.out.println("Aluno " + N_alunos[i] + " Media " + media_alunos[i] + " Situação Reprovado!!!");
-            }
-        }
-
+        Cursos c2 = new Cursos("fatec",3,3);
+        
+        
+        String N_alunos[] = new String[c1.getTotal_alunos()];
+        double notas_alunos[][] = new double[c1.getTotal_alunos()][c1.getBimestre()];
+        double media_alunos[] = new double[c1.getTotal_alunos()];
+        
+        c1.Nomes_alunos(N_alunos);
+        c1.Notas_alunos(notas_alunos,N_alunos);
+        c1.Media_alunos(notas_alunos,N_alunos,media_alunos);
+        c1.resultado(media_alunos,N_alunos);
+        
+        c2.Nomes_alunos(N_alunos);
+        c2.Notas_alunos(notas_alunos, N_alunos);
+        c2.Media_alunos(notas_alunos, N_alunos, media_alunos);
+        c2.status();
+        
+        
+        
     }
 }
