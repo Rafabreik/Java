dd1dc62900cea5882631d9521f6d8a5fd5fb7fda
Rafabreik <72153161+Rafabreik@users.noreply.github.com>
2021-06-15 16:12:30 -0300

    Control project with interface

---
+++ Aula06/src/Control/Control.java
@@ -0,0 +1,16 @@
+
+package Control;
+
+
+public class Control {
+
+    
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        
+        c.abrirMenu();
+        c.maisVolume();
+        c.abrirMenu();
+    }
+    
+}
